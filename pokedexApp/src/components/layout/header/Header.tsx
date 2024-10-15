260492186751b880166fe424e3712a6b29c5e742
Daniel Molina <daniel.molina3192@gmail.com>
2024-10-15 16:54:17 -0500

    sign in sign up buttons

--- pokedexApp/src/components/layout/header/Header.tsx
+++ pokedexApp/src/components/layout/header/Header.tsx
@@ -1,68 +1,73 @@
-import { ReactComponent as PokemonLogo } from "../../../assets/logo-pokemon.svg";
+import { ReactComponent as PokemonLogo } from '../../../assets/logo-pokemon.svg';
 import {
-  useToastContext,
-  clearLocalStorage,
-  getUserInfo,
-} from "../../../utils";
-import { Button } from "../../button/Button";
-import styles from "./header.module.scss";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+	useToastContext,
+	clearLocalStorage,
+	getUserInfo,
+} from '../../../utils';
+import { Button } from '../../button/Button';
+import styles from './header.module.scss';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export const Header = () => {
-  const user = getUserInfo();
-  let location = useLocation();
-  const navigate = useNavigate();
-  const { showToast } = useToastContext();
+	const user = getUserInfo();
+	let location = useLocation();
+	const navigate = useNavigate();
+	const { showToast } = useToastContext();
 
-  const renderButton = (path: string, label: string) => {
-    return (
-      <Link to={path}>
-        <Button
-          buttonStyle="primary"
-          onClick={() => {
-            return;
-          }}
-        >
-          {label}
-        </Button>
-      </Link>
-    );
-  };
+	const renderButton = (path: string, label: string) => {
+		return (
+			<Link to={path}>
+				<Button
+					buttonStyle='primary'
+					onClick={() => {
+						return;
+					}}
+				>
+					{label}
+				</Button>
+			</Link>
+		);
+	};
 
-  const renderUserButton = () => {
-    if (user && location.pathname === "/") {
-      return renderButton("user", "User account");
-    } else if (!user && location.pathname === "/") {
-      return renderButton("user", "Sign in");
-    } else if (user && location.pathname === "/user") {
-      return (
-        <Button
-          buttonStyle="primary"
-          onClick={() => {
-            clearLocalStorage();
-            navigate("/");
-            showToast({
-              isDisplay: true,
-              message: "Log out successful",
-              type: "success",
-            });
-          }}
-        >
-          Log out
-        </Button>
-      );
-    } else {
-      return null;
-    }
-  };
+	const renderUserButton = () => {
+		if (user && location.pathname === '/') {
+			return renderButton('user', 'User account');
+		} else if (!user && location.pathname === '/') {
+			return (
+				<div className={styles.buttons}>
+					{renderButton('user', 'Sign in')}
+					{renderButton('user', 'Sign up')}
+				</div>
+			);
+		} else if (user && location.pathname === '/user') {
+			return (
+				<Button
+					buttonStyle='primary'
+					onClick={() => {
+						clearLocalStorage();
+						navigate('/');
+						showToast({
+							isDisplay: true,
+							message: 'Log out successful',
+							type: 'success',
+						});
+					}}
+				>
+					Log out
+				</Button>
+			);
+		} else {
+			return null;
+		}
+	};
 
-  return (
-    <header className={styles.header}>
-      <Link to={"/"}>
-        <PokemonLogo />
-      </Link>
+	return (
+		<header className={styles.header}>
+			<Link to={'/'}>
+				<PokemonLogo />
+			</Link>
 
-      {renderUserButton()}
-    </header>
-  );
+			{renderUserButton()}
+		</header>
+	);
 };
